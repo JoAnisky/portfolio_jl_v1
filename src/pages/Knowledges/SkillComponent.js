6543ae66ff9bc8748d24c51e9bdf93e4f6d1f80e
JoAnisky <anisky5@gmail.com>
2023-01-31 16:21:29 +0100

    Rename classe catégories de skills

--- src/pages/Knowledges/SkillComponent.js
+++ src/pages/Knowledges/SkillComponent.js
@@ -6,8 +6,8 @@ const SkillComponent = props => {
 
     return (
 
-        <div className="skill-type">
-            <div className='skill-type__title'>
+        <div className="skill-category">
+            <div className='skill-category__title'>
                 <h3>{item.catTitle}</h3>
             </div>
             <div className="skill-components">
@@ -15,8 +15,12 @@ const SkillComponent = props => {
                 item.stack.map((logo, index) => {
                     return(
                         <div key={index} className="skill-components__skill">
-                            <img src={`${logosPath + logo.name}.svg`} className='skill-component__logo' alt={`logo ${logo.name}`} width="50" />
-                            <p className='skill-component__value'>{logo.name}</p>
+                            <div className="skill-components__skill__logo">
+                                <img src={`${logosPath + logo.name}.svg`} className='skill-component__logo' alt={`logo ${logo.name}`} width={logo.width} />
+                            </div>
+                            <div>
+                                <p className='skill-component__value'>{logo.name}</p>
+                            </div>
                         </div>  
                     )
                 })
