16e3be1124e190570fc0d9fc7d1a9f9b2167fa46
JoAnisky <anisky5@gmail.com>
2023-01-31 19:38:43 +0100

    Ajout composant burger logo

---
+++ src/components/MenuBurger.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MenuBurger = () => {
+    return (
+        <div>
+            <div className='menu-burger'>
+                <button className='menu-burger__btn'>
+                    <div className='burger-box'>
+                        <div className='burger-box__inner'>
+
+                        </div>
+                    </div>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default MenuBurger;
