8e30f35850bc567f8c56ada6bea64a3d7f705076
JoAnisky <anisky5@gmail.com>
2023-02-03 09:45:23 +0100

    Refacto liens menu avec useState et map

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,29 @@ import { useState } from 'react';
 
 const Navbar = () => {
 
+    const [links, setLinks] = useState([
+        {id:1, href:"about", title:"A propos"},
+        {id:2, href:"knowledges", title:"Compétences"},
+        {id:3, href:"portfolio", title:"Portfolio"},
+        {id:4, href:"portfolio", title:"Contact"}
+    ])
+
     const [showLinks, setShowLinks] = useState(false);
 
     const handleShowLinks = () => {
         setShowLinks(!showLinks)
     }
 
+    const menuLinks = links.map(link => {
+        return(
+            <li key={link.id} className='navbar__item'>
+                <a href={`/#${link.href}`} className='navbar__link' onClick={handleShowLinks}>{link.title}</a>
+            </li>
+        )
+        
+    })
+        
+    console.log(links)
     return (
         <nav className="navbar">
             <div className="navbar-mobile">
@@ -22,19 +39,20 @@ const Navbar = () => {
             </div>
             <div className={`navbar__container ${showLinks ? "show-nav" : "hide-nav"}`}>
                 <ul className='navbar__links'>
-                    <li className='navbar__item'>
-                        <a href="/#about" className='navbar__link'>A propos</a>
+                    {menuLinks}
+                    {/* <li className='navbar__item'>
+                        <a href="/#about" className='navbar__link' onClick={handleShowLinks}>A propos</a>
                     </li>
                     <li className='navbar__item'>
-                        <a href="/#knowledges" className='navbar__link'>Compétences</a>
+                        <a href="/#knowledges" className='navbar__link' onClick={handleShowLinks}>Compétences</a>
                     </li>                
                     <li className='navbar__item'>
-                        <a href="/#portfolio" className='navbar__link'>Portfolio</a>
+                        <a href="/#portfolio" className='navbar__link' onClick={handleShowLinks}>Portfolio</a>
                     </li>
                     <li className='navbar__item'>
-                        <a href="/#contact" className='navbar__link'>Contact</a>
-                    </li>
-                    <BtnResume/>
+                        <a href="/#contact" className='navbar__link' onClick={handleShowLinks}>Contact</a>
+                    </li> */}
+                    <BtnResume handleShow={handleShowLinks}/>
                 </ul>
 
             </div>
