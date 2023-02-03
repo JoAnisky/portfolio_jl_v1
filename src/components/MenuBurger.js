94dc659a3333790ec5a2006ea08df28940e01395
JoAnisky <anisky5@gmail.com>
2023-02-03 09:46:33 +0100

    Amélioration animation MenuBurger

--- src/components/MenuBurger.js
+++ src/components/MenuBurger.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const MenuBurger = (props) => {
     let {visible, ...handleShow} = props;
-    console.log(visible);
+    
     return (
         <div>
-            <button className={`menu-burger__btn ${visible ? "opened" : "closed"}`}  onClick={props.handleShow}>
+            <button className={`menu-burger__btn ${visible ? "opened" : "closed"}`} onClick={props.handleShow}>
                 <div className='burger-box'>
                     <div className='burger-box__inner'>
                     </div>
