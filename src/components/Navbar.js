2030a890d5c6cc73e2bc56fbe13def265cece117
JoAnisky <anisky5@gmail.com>
2023-02-03 12:03:53 +0100

    Refacto (map) <li> du menu et ajout animation avec useRef et useEffect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,47 @@ import React from 'react';
 import MainLogo from './MainLogo';
 import MenuBurger from './MenuBurger';
 import BtnResume from './BtnResume';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const Navbar = () => {
 
-    const [links, setLinks] = useState([
+    const [showLinks, setShowLinks] = useState(false);
+    const linksRef = useRef([]);
+
+    const linksList  = [
         {id:1, href:"about", title:"A propos"},
         {id:2, href:"knowledges", title:"Compétences"},
         {id:3, href:"portfolio", title:"Portfolio"},
-        {id:4, href:"portfolio", title:"Contact"}
-    ])
-
-    const [showLinks, setShowLinks] = useState(false);
-
+        {id:4, href:"contact", title:"Contact"}
+    ];      
+    
     const handleShowLinks = () => {
-        setShowLinks(!showLinks)
-    }
+        setShowLinks(!showLinks);
+    };
 
-    const menuLinks = links.map(link => {
+    const addToRefsLinks = (elem) => {
+        if(elem && !linksRef.current.includes(elem)){
+            linksRef.current.push(elem);
+        }
+    };
+    
+    useEffect(() => {
+        let animDuration = 0.1;
+        let animDelay = 0.4;
+        linksRef.current.forEach(link => {        
+            link.style.transition = showLinks ? `${(animDelay += 0.05)}s ${animDuration}s all ease-in-out` : `${(animDelay += 0.05)}s ${animDuration}s all in-out-back`;
+            link.style.transform = showLinks ? "translateX(0px)" : "translateX(650px)";
+        })
+    })
+
+    const menuLinks = linksList.map(link => {
         return(
-            <li key={link.id} className='navbar__item'>
+            <li ref={addToRefsLinks} key={link.id} className='navbar__item'>
                 <a href={`/#${link.href}`} className='navbar__link' onClick={handleShowLinks}>{link.title}</a>
             </li>
-        )
-        
-    })
-        
-    console.log(links)
+        );    
+    });
+
     return (
         <nav className="navbar">
             <div className="navbar-mobile">
@@ -40,23 +54,9 @@ const Navbar = () => {
             <div className={`navbar__container ${showLinks ? "show-nav" : "hide-nav"}`}>
                 <ul className='navbar__links'>
                     {menuLinks}
-                    {/* <li className='navbar__item'>
-                        <a href="/#about" className='navbar__link' onClick={handleShowLinks}>A propos</a>
-                    </li>
-                    <li className='navbar__item'>
-                        <a href="/#knowledges" className='navbar__link' onClick={handleShowLinks}>Compétences</a>
-                    </li>                
-                    <li className='navbar__item'>
-                        <a href="/#portfolio" className='navbar__link' onClick={handleShowLinks}>Portfolio</a>
-                    </li>
-                    <li className='navbar__item'>
-                        <a href="/#contact" className='navbar__link' onClick={handleShowLinks}>Contact</a>
-                    </li> */}
                     <BtnResume handleShow={handleShowLinks}/>
                 </ul>
-
             </div>
-
         </nav>
     );
 };
