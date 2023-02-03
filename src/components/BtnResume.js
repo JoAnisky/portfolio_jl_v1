401999f65a6ed78c130bb84f2169f6527cf890bc
JoAnisky <anisky5@gmail.com>
2023-02-03 09:47:05 +0100

    Fonction onclick sur bouton CV

--- src/components/BtnResume.js
+++ src/components/BtnResume.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const BtnResume = () => {
+const BtnResume = (props) => {
+
     return (
         <li className="resume-button" >
-            <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">CV</a>
+            <a href="/cv.pdf" target="_blank" rel="noopener noreferrer"onClick={props.handleShow}>CV</a>
         </li>
     );
 }
