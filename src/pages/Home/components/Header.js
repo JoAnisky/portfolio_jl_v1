f509a26a004a1e639868cb13d4e3c00f4c83356c
JoAnisky <anisky5@gmail.com>
2023-02-04 09:17:10 +0100

    Effet disparition Header avec useState, useEffect

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import Navbar from '../../../components/Navbar';
 
 const Header = () => {
+
+    const [scrollData, setScrollData] = useState({
+        posY : 0,
+        lastY : 0
+    });
+
+    const [hideHeader, setHideHeader] = useState(false);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            setScrollData(prevState => {
+                return {
+                    posY: window.scrollY,
+                    lastY : prevState.posY
+                }
+            })
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+
+    }, []);
+
+    useEffect(() => {
+        scrollData.posY > 85 ? setHideHeader(true) : setHideHeader(false)
+
+        scrollData.lastY < scrollData.posY ? setHideHeader(true) : setHideHeader(false)
+
+    }, [scrollData]);
+
     return (
-        <header className='header-nav'>
+        <header className={hideHeader ? `header-nav hide-header` : `header-nav`}>
             <Navbar/>                     
         </header>
     );
