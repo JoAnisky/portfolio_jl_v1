1f04dfdc51cd04fbf16ea060caa7125d5e7df289
JoAnisky <anisky5@gmail.com>
2023-02-12 10:52:39 +0100

    Ajout schéma de validation du formulaire de contact

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -1,26 +1,23 @@
 import * as yup from "yup";
 
 export const formSchema = yup
-.object().shape({
-    firstName: yup
-    .string()
-    .min(2, "La longueur minimale est de 2 caractères")
-    .max(50)
-    .required("Champ nom requis"),
-    lastName: yup
-    .string()
-    .min(2, "La longueur minimale est de 2 caractères")
-    .max(50)
-    .required("Champ prénom requis"),
-    email: yup
-    .string()
-    .email("Format d'email incorrect")
-    .max(255)
-    .required("Merci d'entrer une adresse mail"),
-    message: yup
-    .string()
-    .required("Veuillez entrer votre message"),
-    createdOn: yup
-    .date()
-    .default(function () {return new Date();})
-});
+    .object().shape({
+        firstname: yup
+        .string()
+        .min(2, "La longueur minimale est de 2 caractères")
+        .max(50)
+        .required("Champ nom requis"),
+        lastname: yup
+        .string()
+        .min(2, "La longueur minimale est de 2 caractères")
+        .max(50)
+        .required("Champ prénom requis"),
+        email: yup
+        .string()
+        .email("Format d'email incorrect")
+        .max(255)
+        .required("Merci d'entrer une adresse mail"),
+        message: yup
+        .string()
+        .required("Veuillez entrer votre message"),
+    });
