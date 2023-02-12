abe6cbb6155fa47f0051ebe7a1921e716f1a17e3
JoAnisky <anisky5@gmail.com>
2023-02-12 10:50:03 +0100

    Ajout script PHP

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,22 +1,35 @@
 <?php
+
     header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Headers: Content-Type');
-    header('Content-type: application/json');
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-    $rest_json = file_get_contents("php://input");
-    $_POST = json_decode($rest_json, true);
+    $data = json_decode(file_get_contents("php://input"));
 
-    if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])){
-        if(!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && !empty($_POST['message'])){
+    $firstName = $data->firstname;
+    $lastName = $data->lastname;
+    $email = $data->email;
+    $message = $data->message;
+
+    // ... 
+    // ici, vous pouvez effectuer les opérations nécessaires pour enregistrer ces données, telles que les stocker dans une base de données ou les envoyer par e-mail
+    // ... 
+
+    echo json_encode(array("message" => "Les données ont été enregistrées avec succès."));
+    echo json_encode(array("nom" => $firstName, "prénom" => $lastName, "email" => $email, "message" => $message));
+
+    if(isset($firstName) && isset($lastName) && isset($email) && isset($email)){
+        if(!empty($firstName) && !empty($lastName) && !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($email)){
             
             // Récupération des données
-            $firstName = htmlspecialchars($_POST['firstname']);
-            $lastName  = htmlspecialchars($_POST['lastname']);
-            $message   = htmlspecialchars($_POST['message']);
-            $date      = htmlspecialchars($_POST['date']);
+            $firstName = htmlspecialchars($firstName);
+            $lastName  = htmlspecialchars($lastName);
+            $message   = htmlspecialchars($email);
 
             // Infos d'envoi
-            $fromMail     = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+            $fromMail     = filter_var($email, FILTER_SANITIZE_EMAIL);
             $toMail       = 'anisky5@gmail.com';
 
             // Message
@@ -34,17 +47,14 @@
 
             $serverResponse = mail($toMail, $header, $object, $message);
             if ($serverResponse === true){
-               	echo json_encode(array(
-                "sent" => true
-                ));
-                echo json_encode($_POST);
+                echo json_encode(["responseServer"=> true, "responseMail"=> true, "responseMessage" => "données reçues, mail OK"]);                
             } else {
-                echo json_encode(["responseServer"=>true, "responseMail"=> false]);
+                echo json_encode(["responseServer"=> true, "responseMail"=> false, "responseMessage" => "données reçues mail NON envoyé"]);
             }
         } else {
-            echojson_encode(["sent" => false, "message" => "Something went wrong"]);
+            echo json_encode(["responseServer" => false, "responseMessage" => "Données vides"]);
         }
     } else {
-        echo json_encode(["responseServer" => false, "responseMessage" => "Rien n'a été reçu"]);
+        echo json_encode(["responseServer" => false, "responseMessage" => "Rien reçu par POST"]);
     }
 ?>
