15813e24fa206fbbeb69a415f6814d28096be89f
JoAnisky <anisky5@gmail.com>
2023-02-13 13:08:06 +0100

    MAJ formSchema

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -2,21 +2,21 @@ import * as yup from "yup";
 
 export const formSchema = yup
     .object().shape({
-        firstname: yup
-        .string()
+        firstname: yup.string()
+        .required("Champ nom requis")
         .min(2, "La longueur minimale est de 2 caractères")
-        .max(50)
-        .required("Champ nom requis"),
-        lastname: yup
-        .string()
+        .matches(/^[aA-zZ\s]+$/, "Pas de chiffres dans ce champ")
+        .max(50),
+        lastname: yup.string()
+        .required("Champ prénom requis")
         .min(2, "La longueur minimale est de 2 caractères")
-        .max(50)
-        .required("Champ prénom requis"),
-        email: yup
-        .string()
-        .email("Format d'email incorrect")
+        .matches(/^[aA-zZ\s]+$/, "Pas de chiffres dans ce champ")
+        .max(50),
+        email: yup.string()
+        .required("Merci d'entrer une adresse mail").matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Format d'email incorrect")
+        .email()
         .max(255)
-        .required("Merci d'entrer une adresse mail"),
+,
         message: yup
         .string()
         .required("Veuillez entrer votre message"),
