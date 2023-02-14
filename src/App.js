2d0debb280cc603498a4940a1d2478fb42e7246e
JoAnisky <anisky5@gmail.com>
2023-02-14 12:32:14 +0100

    suppression sous dossier Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Header from './pages/Home/components/Header';
+import Header from './pages/Home/Header';
 
 import About from './pages/About/About';
 import Knowledges from './pages/Knowledges/Knowledges';
