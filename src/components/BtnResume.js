811025e7f91e3b12f6e67b5c50444a2cd65d8706
JoAnisky <anisky5@gmail.com>
2023-02-16 11:09:45 +0100

    Ajout CV

--- src/components/BtnResume.js
+++ src/components/BtnResume.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import CV from '../resume/CV_jonathan_lore.pdf';
 
 const BtnResume = (props) => {
     const handleShowLinks = props.handleShow;
 
     return (
         <>
-            <a href="/cv.pdf" target="_blank" rel="noopener noreferrer" onClick={handleShowLinks}>CV</a>
+            <a href={CV} target="_blank" rel="noreferrer" onClick={handleShowLinks}>CV</a>
         </>
 
     );
