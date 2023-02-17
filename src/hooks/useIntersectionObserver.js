f75d68903a4aed48a372ef6fee3848649c6dbada
JoAnisky <anisky5@gmail.com>
2023-02-17 10:49:19 +0100

    Ajout d'un hook Intersection observer

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,28 @@
+import {useState, useMemo, useEffect} from 'react';
+
+const useIntersectionObserver = (options, targetRef) => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const callbackFunction = entries => {
+        const[entry] = entries;
+        setIsVisible(entry.isIntersecting);
+    }
+
+    const optionsMemo = useMemo(() => {
+        return options
+    }, [options]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(callbackFunction, optionsMemo);
+        const currentTarget = targetRef.current;
+        if(currentTarget) observer.observe(currentTarget);
+
+        return() => {
+            if (currentTarget) observer.unobserve(currentTarget);
+        }
+    }, [targetRef, optionsMemo]);
+    return isVisible;
+}
+
+export default useIntersectionObserver;
