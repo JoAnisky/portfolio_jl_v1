ed21afa458251db1cefefb240da6c1179a111d95
JoAnisky <anisky5@gmail.com>
2023-02-20 09:50:20 +0100

    Ajout anim aux titres sections

--- src/components/SectionsTitles.js
+++ src/components/SectionsTitles.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import {useRef} from 'react';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
 const SectionsTitles = (props) => {
+    const targetRef = useRef(null);
+    const isVisible = useIntersectionObserver({
+        root: null,
+        rootMargin: '0px',
+        threshold: 0
+    }, targetRef);
+
     return (
-        <div className='sections-titles-container'>
+        <div ref={targetRef} className={`sections-titles-container ${!isVisible ? "" : "slideup-anim"}`}>
             <span className="sections-titles-container__line"></span>
                 <h2 className='sections-titles-container_text'>{props.name}</h2>
             <span className="sections-titles-container__line"></span>
