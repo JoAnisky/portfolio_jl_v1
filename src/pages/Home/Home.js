1920bcb3498d151c285c603f8cf7886fccb18bcc
JoAnisky <anisky5@gmail.com>
2023-02-20 08:01:22 +0100

    Ajout anim texte d'accueil

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,13 @@ const Home = () => {
     return (
         <section className='hello' id="home">
             <div className="hello__container">
-                <div className="fadein-welcome">
+                <div className="slidein-welcome-anim">
                     <p className='hello__hi'>Bonjour, je suis</p>
                 </div>
-                <div className="fadein-welcome">
+                <div className="slidein-welcome-anim">
                     <h1 className="hello__main-title">Jonathan Loré<span className='hello__main-title__dot'>.</span></h1>
                 </div>
-                <div className="fadein-welcome">
+                <div className="slidein-welcome-anim">
                     <h3 className="hello__subtitle">Développeur web Frontend</h3>
                 </div>
             </div>
