2c3b60c784a7f122bb653cfa360cd3ff3a11f13d
JoAnisky <anisky5@gmail.com>
2023-02-24 10:57:44 +0100

    Mise en page mentions légales et ajout liens articles

--- src/pages/Legals.js
+++ src/pages/Legals.js
@@ -5,52 +5,61 @@ export default function Legals() {
   return (
     <>
     <Header/>
-    <div className="legals">
-        <h1>Mentions l&eacute;gales</h1>
-        <h2>1 - &Eacute;dition du site</h2>
-            <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'&eacute;conomie num& eacute;rique, il est pr&eacute;cis&eacute; aux utilisateurs du site internet https://www.jonathanlore.fr l'identit&eacute; des diff&eacute;rents intervenants dans le cadre de sa r&eacute;alisation et de son suivi:</p>
+    <main>
+        <div className="legals">
+            <h1>Mentions l&eacute;gales</h1>
+            <div>
+                <h2>1 - &Eacute;dition du site<span className='hello__main-title__dot'>.</span></h2>
+                <p>En vertu de <a href="https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000801164#LEGIARTI000042038977" className="green-texts">l'article 6 de la loi n° 2004-575 du 21 juin 2004</a> pour la confiance dans l'&eacute;conomie num&eacute;rique, il est pr&eacute;cis&eacute; aux utilisateurs du site internet <span className="bold-text">https://www.jonathanlore.fr</span> l'identit&eacute; des diff&eacute;rents intervenants dans le cadre de sa r&eacute;alisation et de son suivi:</p>
 
-            <p>Propri&eacute;taire du site : Mr LOR&Eacute; Jonathan  - Contact : webmaster@jonathanlore.fr</p>
+                <p><span className="bold-text">Propri&eacute;taire du site</span> : Mr LOR&Eacute; Jonathan  - Contact : <a href="mailto:webmaster@jonathanlore.fr" className="green-texts">webmaster@jonathanlore.fr</a></p>
 
-            <p>Responsable de la publication : Mr LOR&Eacute; Jonathan .</p>
+                <p><span className="bold-text">Responsable de la publication</span> : Mr LOR&Eacute; Jonathan.</p>
 
-            <p>H&eacute;bergeur : Easyhoster Kim Communication Limited, 2003, 20/F, Tower 5, 33 Canton Road, Tsim Sha Tsui, Kowloon, 999077, Hong Kong.</p>
+                <p><span className="bold-text">H&eacute;bergeur</span> : Easyhoster</p>
+                <p>Adresse :  Kim Communication Limited, 2003, 20/F, Tower 5, 33 Canton Road, Tsim Sha Tsui, Kowloon, 999077, Hong Kong.</p>
+            </div>
+            <div>
+                <h2>2 - Propri&eacute;t&eacute; intellectuelle et contrefaçons<span className='hello__main-title__dot'>.</span></h2>
+                <p><span className="bold-text">Jonathan LOR&Eacute;</span>  est propri&eacute;taire des droits de propri&eacute;t&eacute; intellectuelle et d&eacute;tient les droits d’usage sur tous les &eacute;l&eacute;ments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vid&eacute;os, architecture, icônes et sons.</p>
 
-        <h2>2 - Propri&eacute;t&eacute; intellectuelle et contrefaçons.</h2>
-            <p>LOR&Eacute; Jonathan  est propri&eacute;taire des droits de propri&eacute;t&eacute; intellectuelle et d&eacute;tient les droits d’usage sur tous les &eacute;l&eacute;ments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vid&eacute;os, architecture, icônes et sons.</p>
+                <p>Toute reproduction, repr&eacute;sentation, modification, publication, adaptation de tout ou partie des &eacute;l&eacute;ments du site, quel que soit le moyen ou le proc&eacute;d&eacute; utilis&eacute;, est interdite, sauf autorisation &eacute;crite pr&eacute;alable de <span className="bold-text">Jonathan LOR&Eacute;</span>.</p>
 
-            <p>Toute reproduction, repr&eacute;sentation, modification, publication, adaptation de tout ou partie des &eacute;l&eacute;ments du site, quel que soit le moyen ou le proc&eacute;d&eacute; utilis&eacute;, est interdite, sauf autorisation &eacute;crite pr&eacute;alable de Mr LOR&Eacute; Jonathan.</p>
+                <p>Toute exploitation non autoris&eacute;e du site ou de l’un quelconque des &eacute;l&eacute;ments qu’il  contient sera consid&eacute;r&eacute;e comme constitutive d’une contrefaçon et poursuivie conform&eacute;ment aux dispositions des articles <a href="https://www.legifrance.gouv.fr/affichCodeArticle.do?idArticle=LEGIARTI000032655082&cidTexte=LEGITEXT000006069414&dateTexte=20160605" className="green-texts" target="_blank">L.335-2 et suivants du Code de Propri&eacute;t&eacute; Intellectuelle</a>.</p>
+            </div>
+            <div>
+                <h2>3 - Limitations de responsabilit&eacute;<span className='hello__main-title__dot'>.</span></h2>
+                <p><span className="bold-text">Jonathan LOR&Eacute;</span> ne pourra &ecirc;tre tenu pour responsable des dommages directs et indirects caus&eacute;s au mat&eacute;riel de l’utilisateur, lors de l’acc&egrave;s au site <span className="bold-text">https://www.jonathanlore.fr</span>.</p>
 
-            <p>Toute exploitation non autoris&eacute;e du site ou de l’un quelconque des &eacute;l&eacute;ments qu’il  contient sera consid&eacute;r&eacute;e comme constitutive d’une contrefaçon et poursuivie conform&eacute;ment aux dispositions des articles L.335-2 et suivants du Code de Propri&eacute;t&eacute; Intellectuelle.</p>
+                <p><span className="bold-text">Jonathan LOR&Eacute;</span>  d&eacute;cline toute responsabilit&eacute; quant &agrave; l’utilisation qui pourrait &ecirc;tre faite des informations et contenus pr&eacute;sents sur <span className="bold-text">https://www.jonathanlore.fr</span>.</p>
 
-        <h2>3 - Limitations de responsabilit&eacute;.</h2>
-            <p>Mr LOR&Eacute; Jonathan  ne pourra être tenu pour responsable des dommages directs et indirects caus&eacute;s au mat&eacute;riel de l’utilisateur, lors de l’accès au site https://www.jonathanlore.fr.</p>
+                <p><span className="bold-text">Jonathan LOR&Eacute;</span> s’engage &agrave; s&eacute;curiser au mieux le site <span className="bold-text">https://www.jonathanlore.fr</span>, cependant sa responsabilit&eacute; ne pourra &ecirc;tre mise en cause si des donn&eacute;es ind&eacute;sirables sont import&eacute;es et install&eacute;es sur son site &agrave; son insu.</p>
 
-            <p>Mr LOR&Eacute; Jonathan  d&eacute;cline toute responsabilit&eacute; quant à l’utilisation qui pourrait être faite des informations et contenus pr&eacute;sents sur https://www.jonathanlore.fr.</p>
+                <p>Des espaces interactifs (espace contact ou commentaires) sont &agrave; la disposition des utilisateurs. <span className="bold-text">Jonathan LOR&Eacute;</span>  se r&eacute;serve le droit de supprimer, sans mise en demeure pr&eacute;alable, tout  contenu d&eacute;pos&eacute; dans cet espace qui contreviendrait &agrave; la l&eacute;gislation applicable en   France, en particulier aux dispositions relatives &agrave; la protection des donn&eacute;es.</p>
 
-            <p>Mr LOR&Eacute; Jonathan  s’engage à s&eacute;curiser au mieux le site https://www.jonathanlore.fr, cependant sa responsabilit&eacute; ne pourra être mise en cause si des donn&eacute;es ind&eacute;sirables sont import&eacute;es et install&eacute;es sur son site à son insu.</p>
+                <p>Le cas &eacute;ch&eacute;ant, <span className="bold-text">Jonathan LOR&Eacute;</span> se r&eacute;serve &eacute;galement la possibilit&eacute; de mettre en cause la responsabilit&eacute; civile et/ou p&eacute;nale de l’utilisateur, notamment en cas de message &agrave; caract&egrave;re raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilis&eacute; (texte, photographie …).</p>
+            </div>
+            <div>
+                <h2>4 - CNIL et gestion des donn&eacute;es personnelles<span className='hello__main-title__dot'>.</span></h2>
+                <p>Conform&eacute;ment aux dispositions de  <a href="https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=JORFTEXT000000886460 " className="green-texts" target="_blank"> la loi 78-17 du 6 janvier 1978 modifi&eacute;e</a>, l’utilisateur du site <span className="bold-text">https://www.jonathanlore.fr</span> dispose d’un droit d’acc&egrave;s, de modification et de suppression des informations collect&eacute;es. Pour exercer ce droit, envoyez un message au webmaster du site &agrave; l'adresse <a href="mailto:webmaster@jonathanlore.fr"><span className="green-texts">webmaster@jonathanlore.fr</span></a>.</p>
+            </div>
+            <div>
+                <h2>5 - Liens hypertextes et cookies<span className='hello__main-title__dot'>.</span></h2>
+                <p>Le site <span className="bold-text">https://www.jonathanlore.fr</span> contient des liens hypertextes vers d’autres sites et d&eacute;gage toute responsabilit&eacute; &agrave; propos de ces liens externes ou des liens cr&eacute;&eacute;s par d’autres sites vers <span className="bold-text">https://www.jonathanlore.fr</span>.</p>
 
-            <p>Des espaces interactifs (espace contact ou commentaires) sont à la disposition des utilisateurs. Mr LOR&    Eacute; Jonathan  se r&eacute;serve le droit de supprimer, sans mise en demeure pr&eacute;alable, tout  contenu d&eacute;pos&eacute; dans cet espace qui contreviendrait à la l&eacute;gislation applicable en   France, en particulier aux dispositions relatives à la protection des donn&eacute;es.</p>
+                <p>La navigation sur le site <span className="bold-text">https://www.jonathanlore.fr</span> est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.</p>
 
-            <p>Le cas &eacute;ch&eacute;ant, Mr LOR&Eacute; Jonathan  se r&eacute;serve &eacute;galement la possibilit&    eacute; de mettre en cause la responsabilit&eacute; civile et/ou p&eacute;nale de l’utilisateur, notamment  en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilis&eacute; (texte, photographie …).</p>
+                <p>Un "cookie" est un fichier de petite taille qui enregistre des informations relatives &agrave; la navigation d’un utilisateur sur un site. Les donn&eacute;es ainsi obtenues permettent d'obtenir des mesures de fr&eacute;quentation, par exemple.</p>
 
-        <h2>4 - CNIL et gestion des donn&eacute;es personnelles.</h2>
-            <p>Conform&eacute;ment aux dispositions de la loi 78-17 du 6 janvier 1978 modifi&eacute;e, l’utilisateur du site https://www.jonathanlore.fr dispose d’un droit d’accès, de modification et de suppression des informations collect&eacute;es. Pour exercer ce droit, envoyez un message au webmaster du site à l'adresse webmaster@jonathanlore.fr.</p>
+                <p>Vous avez la possibilit&eacute; <span className="bold-text">d’accepter ou de refuser les cookies</span> en modifiant les param&egrave;tres de votre navigateur. Aucun cookie ne sera d&eacute;pos&eacute; sans votre consentement.</p>
 
-        <h2>5 - Liens hypertextes et cookies</h2>
-            <p>Le site https://www.jonathanlore.fr contient des liens hypertextes vers d’autres sites et d&eacute;gage toute responsabilit&eacute; à propos de ces liens externes ou des liens cr&eacute;&eacute;s par d’autres sites vers https://www.jonathanlore.fr.</p>
+                <p>Les cookies sont enregistr&eacute;s pour une dur&eacute;e maximale de 12 mois.</p>
 
-            <p>La navigation sur le site https://www.jonathanlore.fr est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur.</p>
-
-            <p>Un "cookie" est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les donn&eacute;es ainsi obtenues permettent d'obtenir des mesures de fr&eacute;quentation, par exemple.</p>
-
-            <p>Vous avez la possibilit&eacute; d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera d&eacute;pos&eacute; sans votre consentement.</p>
-
-            <p>Les cookies sont enregistr&eacute;s pour une dur&eacute;e maximale de 12 mois.</p>
-
-        <h2>6 - Droit applicable</h2>
-            <p>Tout litige en relation avec l’utilisation du site https://www.jonathanlore.fr est soumis au droit français.</p>
-    </div>
+                <h2>6 - Droit applicable<span className='hello__main-title__dot'>.</span></h2>
+                <p>Tout litige en relation avec l’utilisation du site <span className="bold-text">https://www.jonathanlore.fr</span> est soumis au droit français.</p>
+            </div>
+        </div>
+    </main>
     </>
   )
 }
