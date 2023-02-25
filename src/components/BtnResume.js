543aba753dc05309a43bf37d857bac8dd7c462f9
JoAnisky <anisky5@gmail.com>
2023-02-25 08:13:14 +0100

    Ajout package GA et Analytics btn cv

--- src/components/BtnResume.js
+++ src/components/BtnResume.js
@@ -1,6 +1,8 @@
 import CV from '../resume/CV_jonathan_lore.pdf';
+import useAnalyticsEventTracker from '../hooks/useAnalyticsEventTracker';
 
 const BtnResume = (props) => {
+    const gaEventTracker = useAnalyticsEventTracker('CV');
     const handleShowLinks = props.handleShow;
 
     return (
