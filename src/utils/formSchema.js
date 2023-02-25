f6d9cfc96a9a8c7f00e4703fb568f1715a013665
JoAnisky <anisky5@gmail.com>
2023-02-25 12:27:21 +0100

    Modif Regex et messages formulaire

--- src/utils/formSchema.js
+++ src/utils/formSchema.js
@@ -1,16 +1,20 @@
 import * as yup from "yup";
 
+const firstLastnameMinMessage = "La longueur minimale est de 2 caractères";
+const firstLastnameRegex = /^(?!^[0-9]+$).+$/;
+const firstLastnameMessage = "Pas uniquement des chiffres dans ce champ";
+
 export const formSchema = yup
     .object().shape({
         firstname: yup.string()
         .required("Champ nom requis")
-        .min(2, "La longueur minimale est de 2 caractères")
-        .matches(/^[aA-zZ\s]+$/, "Pas de chiffres dans ce champ")
+        .min(2, firstLastnameMinMessage)
+        .matches(firstLastnameRegex, firstLastnameMessage)
         .max(50),
         lastname: yup.string()
         .required("Champ prénom requis")
-        .min(2, "La longueur minimale est de 2 caractères")
-        .matches(/^[aA-zZ\s]+$/, "Pas de chiffres dans ce champ")
+        .min(2, firstLastnameMinMessage)
+        .matches(firstLastnameRegex, firstLastnameMessage)
         .max(50),
         email: yup.string()
         .required("Merci d'entrer une adresse mail").matches(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Format d'email incorrect")
