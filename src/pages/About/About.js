1619067ea5a7aca31a428a07a2ac437829a01008
JoAnisky <anisky5@gmail.com>
2023-03-02 10:27:47 +0100

    Ajout majuscule 'Je suis'

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
             <SectionsTitles name="À Propos"/>
                 <div className="about__inner">
                     <div className={`about__prez ${!isVisible ? "" : "slidein-anim"}`}>
-                            <p>Bonjour ! je suis <strong>Jonathan Lor&eacute;</strong>, d&eacute;veloppeur web Front end.</p>
+                            <p>Bonjour ! Je suis <strong>Jonathan Lor&eacute;</strong>, d&eacute;veloppeur web Front end.</p>
                             <p>Tomb&eacute; dans le monde du web en 2007, j'ai d&eacute;but&eacute; avec HTML et CSS. Je suis passionn&eacute; par  les nouvelles technologies et le graphisme.</p>
                             <p>J'aime cr&eacute;er des sites web vivants et interactifs.</p>
                             <p>Vous trouverez ici mes diff&eacute;rentes <HashLink className="links" smooth to="/#portfolio">r&eacute;alisations</HashLink> et un formulaire de <HashLink className="links" smooth to="/#contact">contact</HashLink> pour faire connaissance
